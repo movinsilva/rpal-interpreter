5b308d69c57f0c98c0e7aeaf1590f3b9ac310662
Sandev Dewthilina <sandevdewthilina2000@gmail.com>
2023-07-26 19:24:26 +0530

    updates

--- source/csem/Beta.java
+++ source/csem/Beta.java
@@ -5,27 +5,6 @@ import java.util.Stack;
 import ast.ASTNode;
 import ast.ASTNodeType;
 
-/**
- * Used to evaluate conditionals.
- * 'cond -> then | else' in source becomes 'Beta cond' on the control stack
- * where
- * Beta.thenBranch = standardized version of then
- * Beta.elseBranch = standardized version of else
- * 
- * This inversion is key to implementing a program order evaluation
- * (critical for recursion where putting the then and else nodes above the
- * Conditional
- * node on the control stack will cause infinite recursion if the then and else
- * nodes call the recursive function themselves). Putting the cond node before
- * Beta (and, since
- * Beta contains the then and else nodes, effectively before the then and else
- * nodes), allows
- * evaluating the cond first and then (in the base case) choosing the
- * non-recursive option. This
- * allows breaking out of infinite recursion.
- * 
- * @author Raj
- */
 public class Beta extends ASTNode {
   private Stack<ASTNode> thenBranch; // Changed the variable name to thenBranch
   private Stack<ASTNode> elseBranch;
